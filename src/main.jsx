44528c59833a6eb01030d5626fc96894ef7c9f4f
Hazza <hazzaalbadi.om@gmail.com>
2026-06-03 18:32:28 +0400

    adding Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
 
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function App() {
+  return (
+    <div>
+      {/* ... */}
+      <Analytics />
+    </div>
+  );
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
